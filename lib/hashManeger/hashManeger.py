0edeadd0181eac8d8f41a432f284bc0ec14f03d7
Marcos Ramon <marcosramon2897@gmail.com>
2020-04-16 14:21:12 -0300

    refatoracao de leitura e nome de variaveis

--- lib/hashManeger/hashManeger.py
+++ lib/hashManeger/hashManeger.py
@@ -2,24 +2,28 @@ import lib.hashMaker.hashMaker as hashMaker
 import lib.messages.messages as message
 
 
-##=========funcoes
+# #=========funcoes
 
-#gera hash do arquivo de acordo com o nome do hash informado  
-def retornarHashArquivo(hashFile,file,buffersize) -> str:
+# gera hash do arquivo de acordo com o nome do hash informado
+
+
+def retornarHashArquivo(hashName, file, buffersize) -> str:
     """
         retorna hash do  file informado deacordo com o tipo de hash informado.
     """
-    if hashFile == "sha256":
-        return hashMaker.retornarHashSha256(file,buffersize)
-    elif hashFile == "sha512":
-        return hashMaker.retornarHashSha512(file,buffersize)
-    elif hashFile == "md5":
-        return hashMaker.retornarHashMd5(file,buffersize)
+    if hashName == "sha256":
+        return hashMaker.retornarHashSha256(file, buffersize)
+    elif hashName == "sha512":
+        return hashMaker.retornarHashSha512(file, buffersize)
+    elif hashName == "md5":
+        return hashMaker.retornarHashMd5(file, buffersize)
     else:
         return None
 
-#compara hashs do arquivo original e o hash informado
-def comparar_hashs(originalHash,hashFile,isVerbose=False) -> None:
+# compara hashs do arquivo original e o hash informado
+
+
+def comparar_hashs(originalHash, hashFile, isVerbose=False) -> None:
     """
         verifica se hashs informados sao iguais ou diferentes.
     """
@@ -28,5 +32,5 @@ def comparar_hashs(originalHash,hashFile,isVerbose=False) -> None:
     else:
         hashsSaoIguais = False
 
-    message.infoComparacaohashs(hashsSaoIguais,originalHash,hashFile,isVerbose)
+    message.infoComparacaohashs(hashsSaoIguais, originalHash, hashFile, isVerbose)
 
